3c4e75b92b69a1b70670e4dc67eb118d400492ae
Abderrahman El Bissari <ael-biss@c3r9p13.1337.ma>
2026-01-06 01:03:39 +0100

    done

---
+++ a.py
@@ -0,0 +1,20 @@
+a = -6
+print("a", hex(id(a)))
+
+
+b = int("-6")
+
+# c = -5
+# print("c", hex(id(c)))
+print("b", hex(id(b)))
+
+# a ="hello "
+# b = "hello "
+# == 
+print(a.__eq__(b))
+# del c
+
+# print(globals())
+# b += 5
+# print("b", hex(id(b)))
+# print(c)
